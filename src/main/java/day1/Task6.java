72b6e7d29bde5d2629794542bacd5ebe7f7585a3
Daniil Krysov <dankrysoff@gmail.com>
2020-09-21 13:40:17 +0300

    Add day1 task solution

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 7;
+        for (int x = 0; x < 10; x++){
+            System.out.println(x + " x " + k + " = " + x * k);
+        }
+    }
+}
