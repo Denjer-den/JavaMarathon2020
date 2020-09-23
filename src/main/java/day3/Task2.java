a394ed76940001b3133d6b9b64334ce8fac82fa1
Daniil Krysov <dankrysoff@gmail.com>
2020-09-23 15:24:30 +0300

    Add day3 task solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            if (divider == 0) {
+                break;
+            } else {
+                System.out.println(dividend / divider);
+            }
+        }
     }
 }
