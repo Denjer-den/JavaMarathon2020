4ee7740c653f8d77fa5972743cc543e4e70dd64a
Daniil Krysov <dankrysoff@gmail.com>
2020-09-25 12:19:43 +0300

    Add day5 task solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,13 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+Car car = new Car();
+car.setYear(1999);
+car.setModel("Skyline GTR");
+car.setColor("Blue");
 
+        System.out.println(car.getYear());
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,6 +2,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+Motorbike motorbike = new Motorbike(1988, "Red","Юпитер");
+        System.out.println(motorbike.getYear());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getModel());
     }
 }
