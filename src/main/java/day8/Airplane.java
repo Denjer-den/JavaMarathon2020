42e93ead6613ecd15cbdc8940c1edfc10e22584a
Daniil Krysov <dankrysoff@gmail.com>
2020-09-29 15:34:27 +0300

    Add day8 task solution

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,39 @@
+package day8;
+
+public class Airplane {
+    private String manufacturer;
+    private int year;
+    private int length;
+    private int weight;
+    private int quantityFuel = 0;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void info(){
+        System.out.println("Изготовитель:" + manufacturer + ", год выпуска:" + year + ", длина:" + length + ", вес:" + weight);
+    }
+
+    public int fillUp(int quantityFuel){
+        this.quantityFuel += quantityFuel;
+        return this.quantityFuel;
+    }
+
+    @Override
+    public String toString() {
+        return "Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight
+                + ", количество топлива в баке: " + quantityFuel;
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = "";
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 2000; i++) {
+            s = s + i + " ";
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println(finish - start);
+        System.out.println(s);
+
+        StringBuilder str = new StringBuilder();
+
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i <= 2000; i++) {
+            str.append(i + " ");
+        }
+        System.out.println(str);
+        long finish2 = System.currentTimeMillis();
+        System.out.println(finish2 - start2);
 
     }
 }
