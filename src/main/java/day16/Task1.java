a8c05293c20a326f3817e28c7dc6cc21a9bcd1f7
Daniil Krysov <dankrysoff@gmail.com>
2020-10-07 14:15:04 +0300

    Add day 16 task solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,30 @@
 package day16;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("numbers");
+
+        printResult(file);
+    }
+
+    static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String str = scanner.nextLine();
+        String[] arr = str.split(" ");
+
+        int sum = 0;
+        double aSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += Integer.parseInt(arr[i]);
+            if (i == arr.length - 1)
+                aSum = (double) sum / (i + 1);
+        }
+        System.out.printf("%.14f", aSum);
+        System.out.printf(" --> " + "%.3f", aSum);
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,48 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file1 = new File("file1.txt");
+        PrintWriter pw1 = new PrintWriter(file1);
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            pw1.print(random.nextInt(100) + " ");
+        }
+        pw1.close();
+
+        File file2 = new File("file2.txt");
+        PrintWriter pw2 = new PrintWriter(file2);
+        Scanner scanner = new Scanner(file1);
+        String s = scanner.nextLine();
+        String[] str = s.split(" ");
+        int sum = 0;
+        for (int i = 0; i < str.length; i++) {
+            sum += Integer.parseInt(str[i]);
+            if ((i + 1) % 20 == 0) {
+                double aSum = (double) sum / (i + 1);
+                pw2.print(aSum + " ");
+            }
+        }
+        pw2.close();
+
+        printResult(file2);
+    }
+
+    static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String s = scanner.nextLine();
+        String[] str = s.split(" ");
+        double sum = 0;
+        for (int i = 0; i < str.length; i++) {
+            sum += Double.parseDouble(str[i]);
+        }
+        System.out.println((int) sum);
     }
 }
