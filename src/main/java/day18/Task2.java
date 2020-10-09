debaf250da62b36b06e8142ec1c3086b0174079e
Daniil Krysov <dankrysoff@gmail.com>
2020-10-09 20:03:06 +0300

    Add day 18 task solution

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -4,17 +4,20 @@ package day18;
  * @author Neil Alishev
  */
 public class Task2 {
+
     public static void main(String[] args) {
-        System.out.println(count7(717771237)); // 5
+
+        System.out.println(count7(717771237));
     }
 
-    public static int count7(int number) {
-        if (number == 0)
+    public static int count7(int i) {
+
+        if (i % 10 == 7)
+            return 1 + count7(i / 10);
+
+        if (String.valueOf(Math.abs(i)).length() == 1)
             return 0;
 
-        if (number % 10 == 7)
-            return 1 + count7(number / 10);
-        else
-            return count7(number / 10);
+        return count7(i / 10);
     }
 }
